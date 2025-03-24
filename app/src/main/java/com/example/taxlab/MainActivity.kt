d834dfdbed71fb1ea2ef0d3ef048e12c0f4dfe93
LAB <nikhilpareek@jklu.com>
2025-03-24 15:36:45 +0530

    Fixed Bug for Income greater than 24 Lac

--- app/src/main/java/com/example/taxlab/MainActivity.kt
+++ app/src/main/java/com/example/taxlab/MainActivity.kt
@@ -50,36 +50,31 @@ class MainActivity : AppCompatActivity() , View.OnClickListener {
             grossIncome in 400000..800000 -> {
                 val taxAmnt = grossIncome - 300000
                 tax = 5 * taxAmnt / 100f
-                tax += 3 * tax / 100f
             }
             grossIncome in 800001..1200000 -> {
                 val taxAmnt = grossIncome - 800000
-                tax = taxAmnt / 10f + 20000f
-                tax += 3 * tax / 100f
+                tax = taxAmnt * 10 / 100f + 20000f
             }
             grossIncome in 1200001..1600000 -> {
                 val taxAmnt = grossIncome - 1200000
                 tax = taxAmnt * 15 / 100f + 60000f
-                tax += 3 * tax / 100f
             }
             grossIncome in 1600001..2000000 -> {
                 val taxAmnt = grossIncome - 1600000
-                tax = taxAmnt / 5f + 120000f
-                tax += 3 * tax / 100f
+                tax = taxAmnt * 20 / 100f + 120000f
             }
             grossIncome in 2000001..2400000 -> {
                 val taxAmnt = grossIncome - 2000000
-                tax = taxAmnt / 4f + 200000f
-                tax += 3 * tax / 100f
+                tax = taxAmnt * 25 / 100f + 200000f
             }
             else -> {
                 val taxAmnt = grossIncome - 2400000
-                tax = taxAmnt * 3 / 10f + 300000f
-                tax += 3 * tax / 100f
+                tax = taxAmnt * 30 / 100f + 300000f
             }
         }
         return tax
     }
+
 
 
 }
